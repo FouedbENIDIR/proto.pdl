015c3fb24150660fdba043d433813e7e76edb938
BENIDIR Foued <foued.benidir@groupe-esigelec.org>
2023-03-29 12:03:08 +0200

    update window builder

--- src/gui/ConnexionPage.java
+++ src/gui/ConnexionPage.java
@@ -3,10 +3,18 @@ package gui;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.JTextPane;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
 
 public class ConnexionPage {
 
 	private JFrame frame;
+	private JTextField textField;
+	private JTextField textField_1;
 
 	/**
 	 * Launch the application.
@@ -38,6 +46,34 @@ public class ConnexionPage {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		textField = new JTextField();
+		textField.setText("...");
+		textField.setBounds(160, 77, 86, 20);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setText("...");
+		textField_1.setBounds(160, 146, 86, 20);
+		frame.getContentPane().add(textField_1);
+		textField_1.setColumns(10);
+		
+		JButton btnNewButton = new JButton("Connexion");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton.setBounds(157, 207, 89, 23);
+		frame.getContentPane().add(btnNewButton);
+		
+		JLabel lblNewLabel = new JLabel("Identifiant ");
+		lblNewLabel.setBounds(61, 80, 89, 14);
+		frame.getContentPane().add(lblNewLabel);
+		
+		JLabel lblMotDePasse = new JLabel("Mot de passe");
+		lblMotDePasse.setBounds(61, 149, 89, 14);
+		frame.getContentPane().add(lblMotDePasse);
 	}
-
 }

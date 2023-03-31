7f5e025150605ba887e60fa95d921c9d4f61be6e
JezzBefos <129263368+JezzBefos@users.noreply.github.com>
2023-03-31 17:38:56 +0200

    Update Enseignant.java

--- src/MODUL/Enseignant.java
+++ src/MODUL/Enseignant.java
@@ -1,5 +1,30 @@
 package MODUL;
 
-public class Enseignant {
+public class Enseignant extends Personne {
+	private String tel;
+	private String mail;
 
+	public Enseignant(String nom, String prenom, String id, String motDePasse, String tel, String mail) {
+		super(nom, prenom, id, motDePasse);
+		this.tel = tel;
+		this.mail = mail;
+	}
+
+	public String getTel() {
+		return tel;
+	}
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	
+	
 }

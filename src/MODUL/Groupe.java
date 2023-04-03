40169a5ad4c216500c19378df0906394be3558a8
JezzBefos <129263368+JezzBefos@users.noreply.github.com>
2023-04-03 10:34:34 +0200

    Update Groupe.java

--- src/MODUL/Groupe.java
+++ src/MODUL/Groupe.java
@@ -1,5 +1,27 @@
 package MODUL;
 
 public class Groupe {
+	private int identifiant;
+	private String nom;
+	
+	public Groupe(int identifiant, String nom) {
+		this.identifiant = identifiant;
+		this.nom = nom;
+	}
 
+	public int getIdentifiant() {
+		return identifiant;
+	}
+
+	public void setIdentifiant(int identifiant) {
+		this.identifiant = identifiant;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
 }
